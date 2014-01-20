d91bd41b2f16663d0fa26e3475c7aa2af04832bd
Jonathan English <jonathan.english5@gmail.com>
2014-01-19 19:48:33 -0500

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jonbook::Application.config.secret_key_base = 'c42ace3135fc57eab3993e7b9c9632838dc760d8a90722d7efc67266ed7bf569af6df2b448a41c40103be6d77403129088f4335bc4b13ba04e9a6e2de1e0617f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jonbook::Application.config.session_store :cookie_store, key: '_jonbook_session'
